cc3d2b683d014048934581d075402a7933aa32bb
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-03-02 17:07:31 +0200

    add logic/style for Banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from "./Banner.module.css"
+import bannerImg from "../../images/banner.png"
+
+const Banner = () => {
+  return (
+    <section className={s.banner}>
+      <div className={s.left}>
+        <p className={s.content}>new year<span>sale</span></p>
+        
+        <button className={s.more}>See more</button>
+      </div>
+      <div className={s.right} style={{ backgroundImage: `url(${bannerImg})` }}>
+        <p className={s.discount}>save up to <span>50%</span> off</p>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
