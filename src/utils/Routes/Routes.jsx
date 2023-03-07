3c1fcb607dd18a682bed7940b7871348d0f81539
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-03-07 14:20:52 +0200

    create categories

--- src/utils/Routes/Routes.jsx
+++ src/utils/Routes/Routes.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../../components/Home/Home';
 import { ROUTES } from '../routes';
+import Home from '../../components/Home/Home';
 import SingleProduct from '../../components/Products/SingleProduct';
 import Profile from '../../components/Profile/Profile';
+import SinglrCategory from '../../components/Categories/SinglrCategory';
 
 
 const AppRoutes = () => {
@@ -12,6 +13,7 @@ const AppRoutes = () => {
       <Route index element={<Home />} />
       <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
       <Route path={ROUTES.PROFILE} element={<Profile />} />
+      <Route path={ROUTES.CATEGORY} element={<SinglrCategory />} />
     </Routes>
   )
 }
